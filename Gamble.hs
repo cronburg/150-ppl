053af0059462273a19996313e6bdec1d2deed824
Karl Cronburg <karl.cronburg@gmail.com>
2014-10-02 01:51:18 -0400

    finished problems A-L

--- Gamble.hs
+++ Gamble.hs
@@ -3,3 +3,21 @@ import Infer
 import Dice
 import Tally
 
+data Payouts = Win Double Double Double Double
+
+-- Computes expected payout for the guesser for one game
+expPayout :: P D -> Payouts -> Int -> Int -> Double
+expPayout bag (Win g1 g2 g3 g4) throws cutoff =
+  let dst0 :: P (Pair, TallySheet)
+      dst0 = allTallies bag throws cutoff
+      dst1 :: P TallySheet
+      dst1 = pmap (\(pr,ts) -> ts) $ dst0
+      nthProb :: Int -> P Double -- P Dist over probability of most probable pair
+      nthProb n = pmap (\ts -> snd $ mostProb n (pfilter (\(pr,ts') -> ts == ts') dst0)) dst1
+      dst :: P Double
+      dst = P [(g1,expected $ nthProb 0),
+               (g2,expected $ nthProb 1),
+               (g3,expected $ nthProb 2),
+               (g4,expected $ nthProb 3)]
+  in expected dst
+
--- dice.hs
+++ dice.hs
@@ -5,37 +5,40 @@ import Gamble
 import Text.Printf
 import Data.List
 
+allCutoffs :: [(Int,Double)]
+allCutoffs = [(cutoff,(*) 1000 (expPayout classbag (Win 1.00 0.50 0.25 (-0.10)) 30 cutoff)) | cutoff <- [3..27]]
+
+bestCutoff :: Int
+bestCutoff = fst (foldl (\(b,eb) (a,ea) -> if ea > eb then (a,ea) else (b,eb))
+                         (head allCutoffs) (tail allCutoffs))
+
 main = do
   printf "-------- Dice Problems ---------\n"
   printf "A.1) P dist of a D6               = %s\n" (show $ d2d d6)
   printf "A.2) P dist of a D12              = %s\n" (show $ d2d d12)
-  printf "B) P(sum = 11 | d6 && d12)        = %.6f\n" (sumEQ d6 d12 11)
-  printf "C.1) P(d6 && d12 | bag)           = %.6f\n"  (draw2 d6 d12 classbag)
-  printf "C.2) P(d20 && !d20 | bag)         = %.6f\n" (drawOnce d20 classbag)
-  printf "D) P(d6 && d12 && sum = 11 | bag) = %.6f\n" (draw2sum d6 d12 11 classbag)
-  printf "E) P(d6 && d12 | sum = 11)        = %.6f\n" (draw2cndtnl d6 d12 11 classbag)
+  printf "B) P(sum = 11 | d6 && d12)        = %7.4f %%\n" $ 100.0 * (sumEQ d6 d12 11)
+  printf "C.1) P(d6 && d12 | bag)           = %7.4f %%\n" $ 100.0 * (draw2 d6 d12 classbag)
+  printf "C.2) P(d20 && !d20 | bag)         = %7.4f %%\n" $ 100.0 * (drawOnce d20 classbag)
+  printf "D) P(d6 && d12 && sum = 11 | bag) = %7.4f %%\n" $ 100.0 * (draw2sum d6 d12 11 classbag)
+  printf "E) P(d6 && d12 | sum = 11)        = %7.4f %%\n" $ 100.0 * (draw2cndtnl d6 d12 11 classbag)
   printf "-------- Tally Problems --------\n"
   
   --print (singleTally d4 d4 2 8)
-  ----print (singleTally d4 d4 30 8)
   printf "F) P(d4 && d4 && LHS = 3 marks)   = %.4f %%\n"
-         (100 * (jointTally d4 d4 (TS 27 3) classbag 8))
+         ((*) 100 (jointTally d4 d4 (TS 27 3) classbag 8))
   printf "G) Expected(RIGHT marks)          = %.2f\n"
          (expectedMarks classbag RIGHT 30 8)
 
-  --printf "G) Expected number of marks in RIGHT column = %.2f\n" (avgRightMarks classbag 30 8)
-  
-  
   printf "-------- Gambling Problems -----\n"
-  --print (expPayout classbag 30 8 1.00 0.50 0.25 (-0.10))
-  
-  --printf "H) Expected payout for Guesser after one game = $ %.4f\n" (expPayout classbag 30 8 1.00 0.50 0.25 (0 - 0.10))
-  --printf "I) Expected payout for Guesser after one game = $ %.4f\n" (expPayout classbag 50 8 1.00 0.50 0.25 (0 - 0.10))
-  
-  --print $ weight (tallyDist classbag 1 8)
-  
-  --print (expPayoutDist 3 classbag 30 8 100.00 0.50 0.25 (-0.10))
-
-  --print (pfilter (\a,b -> a+b) (show $ bind (d2d d6) (d2d d12))
-  -- printf "B) P(sum = 11 | d6 && d12) = %.4f"
+  printf "H) Guesser has better expectation (see part I)\n"
+  let payout30 = (*) 1000 (expPayout classbag (Win 1.00 0.50 0.25 (-0.10)) 30 8)
+  printf "I) Guesser wins after 1,000 games : $ %.2f\n" (payout30)
+  let payout50 = (*) 1000 (expPayout classbag (Win 1.00 0.50 0.25 (-0.10)) 50 8)
+  printf "J) Thrower should charge guesser  : $ %.2f   ( = %.2f - %.2f )\n"
+         (payout50 - payout30) payout50 payout30
+  let cutoff10 = (*) 1000 (expPayout classbag (Win 1.00 0.50 0.25 (-0.10)) 30 10)
+  printf "K) Guesser should at most pay     : $ %.2f   ( = %.2f - %.2f )\n"
+         (cutoff10 - payout30) cutoff10 payout30
+  printf "L) Best split for guesser         = %d" (bestCutoff)
+  --print (allCutoffs)
 
