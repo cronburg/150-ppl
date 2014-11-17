577efb0e9a0bc9aa70a42a9897774c8bfcd84d8a
Karl Cronburg <karl.cronburg@gmail.com>
2014-11-16 19:19:08 -0500

    cellar unit test

--- deckbuild/hakaru/Examples/UnitTests.hs
+++ deckbuild/hakaru/Examples/UnitTests.hs
@@ -7,6 +7,7 @@ import Game.DeckBuild.Dominion.Types
 import Game.DeckBuild.Dominion.Lib
 import Game.DeckBuild.Dominion.Base
 import Examples.Base
+import Examples.Greedy
 import Control.Monad.State
 import Test.HUnit hiding (test)
 -- TODO: setup modules such that they export other modules required to use them
@@ -18,6 +19,7 @@ tests = TestList
   [ TestLabel "dMH" dmh0_test
   , TestLabel "dMH" dmh1_test
   , TestLabel "dMH" dmh2_test
+  , TestLabel "Cellar" cellar0_test
   ]
 mkTest   s e r = TestCase $ assertEqual s e r
 mkTestIO s e r = TestCase $ assertEqual s e (unsafePerformIO r)
@@ -38,4 +40,10 @@ dmh2_result  = dMH $ dBG { p1 = (p1 dBG) { hand = ((hand.p1) dBG) {cards=[COPPER
 dmh2_expects = Just COPPER
 dmh2_test    = mkTestIO "dMH" dmh2_expects dmh2_result
 
+cellar0_result  = evalStateT (do
+  { draw 8; baseCardEffects CELLAR; g <- get
+  ; return $ (cards.hand.p1 $ g, cards.deck.p1 $ g, cards.discardPile.p1 $ g, numActions.p1 $ g)
+  }) greedyGame
+cellar0_expects = ([ESTATE,COPPER,COPPER,COPPER,COPPER,COPPER,COPPER,COPPER], [ESTATE], [ESTATE], 2)
+cellar0_test    = mkTestIO "Cellar" cellar0_expects cellar0_result
 
