c7b5cc9fde84382fcc683e147fde2a44deef679b
Karl Cronburg <karl.cronburg@gmail.com>
2014-11-26 17:05:22 -0500

    rejection sampling plotting code

--- proj/pickleMe.py
+++ proj/pickleMe.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
-from pylab import *
 
-files = ["out%02d.dat"%(i,) for i in range(5)]
+files = ["data/out%02d.dat"%(i,) for i in range(5)]
 for f in files: execfile(f)
 data = x00 + x01 + x02 + x03 + x04
 
 import pickle
-pickle.dump(data, open("data.pkl",'w'))
+pickle.dump(data, open("data/data.pkl",'w'))
 
---
+++ proj/plotMe.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import pickle
+from pylab import *
+
+# Optional percentages on y-axis:
+#to_percentage = lambda y, pos: str(round( ( y / float(len(data)) ) * 100.0, 2)) + '%'
+#plt.gca().yaxis.set_major_formatter(FuncFormatter(to_percentage))
+
+data = pickle.load(open("data/data.pkl",'r'))
+param0,turn = transpose(data)
+
+for t in range(25, 45 + 1):
+  hist_data = param0[where(turn == t)[0]]
+  weights = np.ones_like(hist_data) / len(hist_data)
+  
+  hist(hist_data, bins=30, color='g', weights=weights)
+  xlim(0.0, 1.0)
+  ylim(0.0, 0.12)
+  
+  xlabel("param0 (Village-to-Chancellor buy ratio)")
+  ylabel("probability")
+  title("P(param0 | turns == %02d)" % (t,))
+  savefig("village-chancellor-%02dturns.png" % (t,))
+  close()
+
