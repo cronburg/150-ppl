829d02ecb8a4cea573730e7a8bb835e386dae377
Karl Cronburg <karl.cronburg@gmail.com>
2014-12-10 14:01:49 -0500

    final edits to presentation

--- proj/plotMe.py
+++ proj/plotMe.py
@@ -11,15 +11,17 @@ param0,turn = transpose(data)
 
 for t in range(25, 45 + 1):
   hist_data = param0[where(turn == t)[0]]
-  weights = np.ones_like(hist_data) / len(hist_data)
+  weights = np.ones_like(hist_data) #/ len(hist_data)
   
   hist(hist_data, bins=30, color='g', weights=weights)
   xlim(0.0, 1.0)
-  ylim(0.0, 0.12)
+  ylim(0,250)
+  #ylim(0.0, 0.12)
   
   xlabel("param0 (Village-to-Chancellor buy ratio)")
-  ylabel("probability")
+  #ylabel("probability")
+  ylabel("frequency")
   title("P(param0 | turns == %02d)" % (t,))
-  savefig("village-chancellor-%02dturns.png" % (t,))
+  savefig("data/village-chancellor-%02dturns.png" % (t,))
   close()
 
